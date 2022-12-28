edd448f83088bcc9de97ec98a4d7fb58fdc4f325
Harsh <harshshrivastava554@gmail.com>
2022-12-28 17:28:47 +0530

    Intial Implementation model

--- MainCode.cpp
+++ MainCode.cpp
@@ -1,4 +1,39 @@
-#include<iostream>
+#include <iostream>
+#include <stdio.h>
+#include <cstdio>
+#include <windows.h> //clean the terminal window
+#include <conio.h>   //to use automatic enter and sleep inbuit function
+#include <string>    //exit
+#include <string.h>
+#include <fstream>
+#include <stack> 
+#include <algorithm>
+using namespace std;
+
+class Text{
+private:
+    string Input;
+    string text;
+    string Output;
+
+public:
+    Text(){};
+    class Logic{
+    private:
+        int AlgoName;
+        int AlgoType;
+        string InputBuffer;    
+    
+    public:
+        void Algo1(){
+
+        };
+        void Algo2(){
+
+        };
+    };
+};
+
 
 int main(){
 
