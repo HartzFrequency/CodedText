ea2d511e797a4dbb3bec7e5259f77a871a1c8a4a
Harsh <harshshrivastava554@gmail.com>
2023-01-01 19:45:36 +0530

    morse code day 1

--- MainCode.cpp
+++ MainCode.cpp
@@ -10,30 +10,40 @@
 #include <algorithm>
 using namespace std;
 
-class Text{
-private:
-    string Input;
-    string text;
-    string Output;
+class Text;
 
-public:
-    Text(){};
-    class Logic{
+class Logic{
     private:
-        int AlgoName;
-        int AlgoType;
+        int AlgoType=0;
+            //AlgoType 1 refers to the algorithm those work on the char conversion
+            //AlgoType 2 refers to the algorithm those work on the word by word conversion
+        int ResReq=0;
+            //ResReq{Resource required}
+            //ResReq=1 (usually for algo type 1) which do char to char conversion
+                //A file is available in res folder having conversion written on it
         string InputBuffer;    
     
     public:
-        void Algo1(){
-
+        friend class Text;
+        //algo 1 _>MORSE CODE
+        char MorseCode(char IN){
+            AlgoType=1;
+            
         };
-        void Algo2(){
 
-        };
-    };
 };
 
+class Text{
+    private:
+        string AlgoName;
+        string Input;
+        string text;
+        string Output;
+
+    public:
+        Text(){};
+
+};
 
 int main(){
 
